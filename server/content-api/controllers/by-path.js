762914c93d4589e3534379ee8d7263de8de90d7a
Boaz Poolman <boaz_skater@hotmail.com>
2022-06-04 12:48:14 +0200

    feat: Better public endpoint response format

--- server/content-api/controllers/by-path.js
+++ server/content-api/controllers/by-path.js
@@ -1,6 +1,10 @@
 'use strict';
 
+const _ = require('lodash');
+const { transformResponse } = require('@strapi/strapi/lib/core-api/controller/transform');
+
 const { getPluginService } = require('../../util/getPluginService');
+const { sanitizeOutput } = require('../../util/sanitizeOutput');
 
 /**
  * ByPath controller
@@ -9,27 +13,40 @@ const { getPluginService } = require('../../util/getPluginService');
 module.exports = {
   get: async (ctx) => {
     const { path } = ctx.query;
+    const { auth } = ctx.state;
+    let excludeDrafts = false;
 
     const pathEntity = await getPluginService('pathService').findByPath(path);
     if (!pathEntity) {
-      ctx.status = 404;
-      ctx.body = "Entity not found";
+      ctx.notFound();
       return;
+    }
+
+    // Check drafAndPublish setting.
+    const contentType = strapi.contentTypes[pathEntity.contenttype];
+    if (_.get(contentType, ['options', 'draftAndPublish'], false)) {
+      excludeDrafts = true;
     }
 
     const entity = await strapi.entityService.findMany(pathEntity.contenttype, {
       filters: {
         path_id: pathEntity.id,
+        published_at: excludeDrafts ? {
+          $notNull: true,
+        } : {},
       },
       limit: 1,
     });
     if (!entity[0]) {
-      ctx.status = 404;
-      ctx.body = "Entity not found";
+      ctx.notFound();
       return;
     }
 
+    // Add content type to response.
     entity[0].contentType = pathEntity.contenttype;
-    ctx.send(entity[0]);
+
+    // Format response.
+    const sanitizedEntity = await sanitizeOutput(entity[0], contentType, auth);
+    ctx.body = transformResponse(sanitizedEntity, {}, { contentType });
   },
 };
