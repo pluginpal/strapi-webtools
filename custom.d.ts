3b93974e019a3df954dc4a83ce0962564006793b
Alexander Nortung <alexander.nortung@oakdigital.dk>
2023-06-23 14:11:03 +0200

    chore(deps): Updated @strapi dependencies

--- custom.d.ts
+++ custom.d.ts
@@ -1,5 +1,1 @@
-declare module '@strapi/design-system/*';
-declare module '@strapi/design-system';
-declare module '@strapi/icons';
-declare module '@strapi/icons/*';
 declare module '@strapi/helper-plugin';
