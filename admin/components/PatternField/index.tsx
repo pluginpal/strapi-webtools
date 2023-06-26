6dd377fa17e7d5f0eb4babc835944626d752a223
Alexander Nortung <alex_nortung@live.dk>
2023-06-26 15:11:24 +0200

    feat(PatternField): Will now show loader if fields are not ready

--- admin/components/PatternField/index.tsx
+++ admin/components/PatternField/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef, FC } from "react";
 import { useIntl } from "react-intl";
 import styled from "styled-components";
 
-import { TextInput, Popover, Stack, Box } from "@strapi/design-system";
+import { TextInput, Popover, Stack, Box, Loader } from "@strapi/design-system";
 import { request } from "@strapi/helper-plugin";
 import { useQuery } from "react-query";
 
@@ -27,7 +27,7 @@ const PatternField: FC<Props> = ({
   const patternRef = useRef<HTMLDivElement>(null);
   const { formatMessage } = useIntl();
 
-  const { data: allowedFields } = useQuery<Record<string, string[]>>(
+  const { data: allowedFields, isLoading: allowedFieldsLoading, isError } = useQuery<Record<string, string[]>>(
     ['url-alias', 'pattern', 'allowed-fields'],
     () => request(`/url-alias/pattern/allowed-fields`, { method: "GET" }),
     {
@@ -72,7 +72,14 @@ const PatternField: FC<Props> = ({
     return base + suffix;
   };
 
-  if (!allowedFields) return null;
+
+  if (allowedFieldsLoading) {
+    return <Loader>{formatMessage({ id: 'url-alias.settings.loading', defaultMessage: "Loading content..." })}</Loader>;
+  }
+
+  if (isError || !allowedFields) {
+    return <div>{formatMessage({ id: 'url-alias.pattern.allowedFields.fetchError', defaultMessage: "An error occurred while fetching allowed fields" })}</div>;
+  }
 
   return (
     <div>
