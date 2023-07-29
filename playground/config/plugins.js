e80d3ad4a61d531eba0a3d1772f449f45c2dd1a0
Boaz Poolman <boaz_skater@hotmail.com>
2023-07-29 17:46:10 +0200

    feat: Install addons in playground

--- playground/config/plugins.js
+++ playground/config/plugins.js
@@ -5,4 +5,19 @@ module.exports = {
     enabled: true,
     resolve: path.resolve(__dirname, '../src/plugins/webtools'),
   },
+
+  'webtools-addon-sitemap': {
+    enabled: true,
+    resolve: path.resolve(__dirname, '../src/plugins/webtools-addon-sitemap'),
+  },
+
+  'webtools-addon-redirects': {
+    enabled: true,
+    resolve: path.resolve(__dirname, '../src/plugins/webtools-addon-redirects'),
+  },
+
+  'webtools-addon-menus': {
+    enabled: true,
+    resolve: path.resolve(__dirname, '../src/plugins/webtools-addon-menus'),
+  },
 };
