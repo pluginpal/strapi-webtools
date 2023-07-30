29ecf8236135e6f7cd867ffb37d4e211a5cb845e
Boaz Poolman <boaz_skater@hotmail.com>
2023-07-30 13:55:27 +0200

    feat: Allow to collapse the URL alias sidebar

--- packages/core/admin/components/EditView/index.tsx
+++ packages/core/admin/components/EditView/index.tsx
@@ -12,12 +12,16 @@ import {
   Checkbox,
   Link,
   Loader,
+  Icon,
+  Flex
 } from "@strapi/design-system";
+import { Play } from '@strapi/icons';
 
 import Permalink from './Permalink';
 import getTrad from "../../helpers/getTrad";
 
 const EditView = () => {
+  const [opened, setOpened] = useState<boolean>(false);
   const [contentTypes, setContentTypes] = useState([]);
   const { formatMessage } = useIntl();
   const { modifiedData, onChange, slug } = useCMEditViewDataManager();
@@ -56,95 +60,108 @@ const EditView = () => {
         borderColor="neutral150"
         hasRadius
         padding={4}
-        paddingTop={6}
         shadow="tableShadow"
+        onClick={() => !opened && setOpened(true)}
       >
-        <Typography
-          textColor="neutral600"
-          variant="sigma"
-          id="webtools-sidebar-title"
-        >
-          {formatMessage({
-            id: getTrad("plugin.name"),
-            defaultMessage: "Webtools",
-          })}
-        </Typography>
-        <Box paddingTop={2} paddingBottom={6}>
-          <Divider />
-        </Box>
-        <Stack size={2}>
-          {isLoading ? (
-            <Box>
-              <Loader>
-                {formatMessage({
-                  id: "webtools.settings.loading",
-                  defaultMessage: "Loading content...",
-                })}
-              </Loader>
+        <Flex onClick={() => opened && setOpened(false)}>
+          <Typography
+            textColor="neutral600"
+            variant="sigma"
+            id="webtools-sidebar-title"
+          >
+            {formatMessage({
+              id: getTrad("plugin.name"),
+              defaultMessage: "URL alias",
+            })}
+          </Typography>
+          <Icon
+            width="0.5rem"
+            height="0.5rem"
+            as={Play}
+            transform={!opened ? "rotate(90deg)" : "rotate(-90deg)"}
+            marginLeft="auto"
+          />
+        </Flex>
+        {opened && (
+          <>
+            <Box paddingTop={2} paddingBottom={6}>
+              <Divider />
             </Box>
-          ) : (
-            <Box>
-              <Box>
-                <Checkbox
-                  onValueChange={(value: any) => {
-                    onChange({ target: { name: "path_generated", value } });
-                    if (!("path_value" in modifiedData)) {
-                      onChange({
-                        target: {
-                          name: "path_value",
-                          value: pathEntity.url_path,
-                        },
-                      });
-                    }
-                  }}
-                  value={
-                    "path_generated" in modifiedData
-                      ? modifiedData.path_generated
-                      : "generated" in pathEntity
-                        ? pathEntity.generated
-                        : true
-                  }
-                  name="generated"
-                  hint="Uncheck this to create a custom alias below."
-                >
-                  {formatMessage({
-                    id: getTrad("EditView.ExcludeFromSitemap"),
-                    defaultMessage: " Generate automatic URL alias",
-                  })}
-                </Checkbox>
-                <Link to="/settings/webtools/patterns">
-                  Configure URL alias patterns.
-                </Link>
-              </Box>
-              <Box paddingTop={4}>
-                <TextInput
-                  label="URL alias"
-                  name="path"
-                  hint='Specify a path by which this data can be accessed in the browser. For example, type "/about" when writing an about page.'
-                  disabled={
-                    "path_generated" in modifiedData
-                      ? modifiedData.path_generated
-                      : "generated" in pathEntity
-                        ? pathEntity.generated
-                        : true
-                  }
-                  onChange={async (e: any) => {
-                    if (e.target.value.match(/^[A-Za-z0-9-_.~[\]/]*$/)) {
-                      onChange({
-                        target: { name: "path_value", value: e.target.value },
-                      });
-                    }
-                  }}
-                  value={
-                    "path_value" in modifiedData
-                      ? modifiedData.path_value
-                      : pathEntity.url_path
-                  }
-                />
-              </Box>
-            </Box>
-          )}
-        </Stack>
+            <Stack size={2}>
+              {isLoading ? (
+                <Box>
+                  <Loader>
+                    {formatMessage({
+                      id: "webtools.settings.loading",
+                      defaultMessage: "Loading content...",
+                    })}
+                  </Loader>
+                </Box>
+              ) : (
+                <Box>
+                  <Box>
+                    <Checkbox
+                      onValueChange={(value: any) => {
+                        onChange({ target: { name: "path_generated", value } });
+                        if (!("path_value" in modifiedData)) {
+                          onChange({
+                            target: {
+                              name: "path_value",
+                              value: pathEntity.url_path,
+                            },
+                          });
+                        }
+                      }}
+                      value={
+                        "path_generated" in modifiedData
+                          ? modifiedData.path_generated
+                          : "generated" in pathEntity
+                            ? pathEntity.generated
+                            : true
+                      }
+                      name="generated"
+                      hint="Uncheck this to create a custom alias below."
+                    >
+                      {formatMessage({
+                        id: getTrad("EditView.ExcludeFromSitemap"),
+                        defaultMessage: " Generate automatic URL alias",
+                      })}
+                    </Checkbox>
+                    <Link to="/settings/webtools/patterns">
+                      Configure URL alias patterns.
+                    </Link>
+                  </Box>
+                  <Box paddingTop={4}>
+                    <TextInput
+                      label="URL alias"
+                      name="path"
+                      hint='Specify a path by which this data can be accessed in the browser. For example, type "/about" when writing an about page.'
+                      disabled={
+                        "path_generated" in modifiedData
+                          ? modifiedData.path_generated
+                          : "generated" in pathEntity
+                            ? pathEntity.generated
+                            : true
+                      }
+                      onChange={async (e: any) => {
+                        if (e.target.value.match(/^[A-Za-z0-9-_.~[\]/]*$/)) {
+                          onChange({
+                            target: { name: "path_value", value: e.target.value },
+                          });
+                        }
+                      }}
+                      value={
+                        "path_value" in modifiedData
+                          ? modifiedData.path_value
+                          : pathEntity.url_path
+                      }
+                    />
+                  </Box>
+                </Box>
+              )}
+            </Stack>
+          </>
+        )}
       </Box>
       <Permalink
         path={
