39cbab13beeea9bbf8cae47af90d3f4f515e9e00
Alexander Nortung <alexander.nortung@oakdigital.dk>
2023-09-07 17:54:34 +0200

    feat: Added types to servies through getPluginService()

--- server/util/getPluginService.ts
+++ server/util/getPluginService.ts
@@ -1,11 +1,17 @@
 "use strict";
 
 import { pluginId } from "./pluginId";
+import type config from '..';
 
+type Config = typeof config;
+type Services = Config['services'];
 /**
  * A helper function to obtain a plugin service.
  * @param {string} name The name of the service.
  *
  * @return {any} service.
  */
-export const getPluginService = (name: string) => strapi.plugin(pluginId).service(name);
+export const getPluginService = <ServiceName extends keyof Services>(name: ServiceName) => {
+  const service = strapi.service<ReturnType<Services[ServiceName]>>(`plugin::${pluginId}.${name}`);
+  return service as Exclude<typeof service, undefined>;
+};
