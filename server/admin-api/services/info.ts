b402ecf3e586e571efb20e1ca843b67113091e19
Alexander Nortung <alex_nortung@live.dk>
2023-10-14 23:19:42 +0200

    fix(types): fix types related to strapi 4.14

--- server/admin-api/services/info.ts
+++ server/admin-api/services/info.ts
@@ -6,7 +6,7 @@ import { pluginId } from '../../util/pluginId';
 
 export default () => ({
   getPluginOptions: (uid: string) => {
-    const contentType = strapi.contentType(uid);
+    const contentType = strapi.contentType(uid as any); // Using as any should be fine here as we do not know about the user's content types
     const pluginOptions = get(contentType, ['pluginOptions', pluginId], {});
     const schema = pluginOptionsSchema;
 
