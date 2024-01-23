3d7481e019508d1d1bb419cd6664ce30dff5d6b0
Alexander Nortung <alexander.nortung@oakdigital.dk>
2024-01-23 10:05:58 +0100

    chore(playground): Fixed database path
    The database path in playground needs to escape the dist folder so we
    need to add an extra ".."

--- playground/config/database.ts
+++ playground/config/database.ts
@@ -4,7 +4,13 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'sqlite',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      filename: path.join(
+        __dirname,
+        '..',
+        // We need to go back once more to get out of the dist folder
+        '..',
+        env('DATABASE_FILENAME', '.tmp/data.db')
+      ),
     },
     useNullAsDefault: true,
   },
