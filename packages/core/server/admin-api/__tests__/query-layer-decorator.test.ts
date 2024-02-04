9b1c4a991d4bcb295b1b31acb1317e0fe42e5823
Boaz Poolman <boaz_skater@hotmail.com>
2024-02-04 15:28:01 +0100

    fix: merge conflicts

---
+++ packages/core/server/admin-api/__tests__/query-layer-decorator.test.ts
@@ -0,0 +1,175 @@
+import request from 'supertest';
+// @ts-ignore
+// eslint-disable-next-line import/no-relative-packages
+import { setupStrapi, stopStrapi } from '../../../../../playground/tests/helpers';
+
+beforeAll(async () => {
+  await setupStrapi();
+});
+
+afterAll(async () => {
+  await stopStrapi();
+});
+
+describe('Query layer decorator', () => {
+  it('Create - Should generate a new URL alias', async () => {
+    const page = await strapi.entityService.create("api::test.test", {
+      data: {
+        title: 'Some amazing new page',
+      },
+      populate: ['url_alias']
+    });
+
+    expect(page).toHaveProperty('url_alias.url_path', '/page/some-amazing-new-page');
+    expect(page).toHaveProperty('url_alias.generated', true);
+    expect(page).toHaveProperty('url_alias.contenttype', 'api::test.test');
+  });
+
+  it('Create - Should re-generate a pre-created URL alias if generated is set to true', async () => {
+    const alias = await strapi.entityService.create("plugin::webtools.url-alias", {
+      data: {
+        url_path: '/generated-pre-created-path',
+        generated: true,
+        contenttype: 'api::test.test',
+      },
+    });
+
+    const page = await strapi.entityService.create("api::test.test", {
+      data: {
+        title: 'Generated amazing new page',
+        url_alias: alias.id,
+      },
+      populate: ['url_alias']
+    });
+
+    expect(page).toHaveProperty('url_alias.id', alias.id);
+    expect(page).toHaveProperty('url_alias.url_path', '/page/generated-amazing-new-page');
+    expect(page).toHaveProperty('url_alias.generated', true);
+    expect(page).toHaveProperty('url_alias.contenttype', 'api::test.test');
+  });
+
+  it('Create - Should not re-generate a pre-created URL alias if generated is set to false', async () => {
+    const alias = await strapi.entityService.create("plugin::webtools.url-alias", {
+      data: {
+        url_path: '/pre-created-path',
+        generated: false,
+        contenttype: 'api::test.test',
+      },
+    });
+
+    const page = await strapi.entityService.create("api::test.test", {
+      data: {
+        title: 'Some amazing new page',
+        url_alias: alias.id,
+      },
+      populate: ['url_alias']
+    });
+
+    expect(page).toHaveProperty('url_alias.id', alias.id);
+    expect(page).toHaveProperty('url_alias.url_path', '/pre-created-path');
+    expect(page).toHaveProperty('url_alias.generated', false);
+  });
+
+  it('Update - Should generate a new URL alias if none is present', async () => {
+    const page = await strapi.entityService.create("api::test.test", {
+      data: {
+        title: 'Some about to be updated new page',
+      },
+      populate: ['url_alias']
+    });
+
+    const oldAliasId = page.url_alias.id;
+
+    // Delete the created url alias to make sure none is present
+    // at the time of running the .update() query.
+    await strapi.entityService.delete("plugin::webtools.url-alias", oldAliasId);
+
+    const updatedPage = await strapi.entityService.update("api::test.test", page.id, {
+      data: {
+        // @ts-ignore
+        title: 'Some updated page',
+      },
+      populate: ['url_alias']
+    });
+
+    expect(updatedPage?.url_alias?.id).not.toBe(oldAliasId);
+    expect(updatedPage).toHaveProperty('url_alias.url_path', '/page/some-updated-page');
+    expect(updatedPage).toHaveProperty('url_alias.generated', true);
+    expect(updatedPage).toHaveProperty('url_alias.contenttype', 'api::test.test');
+  });
+
+  it('Update - Should re-generate an existing URL alias if generated is set to true', async () => {
+    const page = await strapi.entityService.create("api::test.test", {
+      data: {
+        title: 'Some about to be updated new page',
+      },
+      populate: ['url_alias']
+    });
+
+    expect(page).toHaveProperty('url_alias.url_path', '/page/some-about-to-be-updated-new-page')
+    expect(page).toHaveProperty('url_alias.generated', true);
+
+    const updatedPage = await strapi.entityService.update("api::test.test", page.id, {
+      data: {
+        // @ts-ignore
+        title: 'Some updated page with overwritten url alias',
+      },
+      populate: ['url_alias']
+    });
+
+    expect(updatedPage).toHaveProperty('url_alias.id', page.url_alias.id);
+    expect(updatedPage).toHaveProperty('url_alias.url_path', '/page/some-updated-page-with-overwritten-url-alias');
+    expect(updatedPage).toHaveProperty('url_alias.generated', true);
+  });
+
+  it('Update - Should not re-generate an existing URL alias if generated is set to false', async () => {
+    const alias = await strapi.entityService.create("plugin::webtools.url-alias", {
+      data: {
+        url_path: '/path-should-not-update',
+        generated: false,
+        contenttype: 'api::test.test',
+      },
+    });
+
+    const page = await strapi.entityService.create("api::test.test", {
+      data: {
+        title: 'Some about to be updated new page',
+        url_alias: alias.id,
+      },
+      populate: ['url_alias']
+    });
+
+    expect(page).toHaveProperty('url_alias.url_path', '/path-should-not-update')
+    expect(page).toHaveProperty('url_alias.generated', false);
+
+    const updatedPage = await strapi.entityService.update("api::test.test", page.id, {
+      data: {
+        // @ts-ignore
+        title: 'Some updated page',
+      },
+      populate: ['url_alias']
+    });
+
+    expect(updatedPage).toHaveProperty('url_alias.id', page.url_alias.id);
+    expect(updatedPage).toHaveProperty('url_alias.url_path', '/path-should-not-update');
+    expect(updatedPage).toHaveProperty('url_alias.generated', false);
+  });
+
+  it('Delete - Should delete the corresponding URL alias as wel', async () => {
+    const page = await strapi.entityService.create("api::test.test", {
+      data: {
+        title: 'Some about to be deleted new page',
+      },
+      populate: ['url_alias']
+    });
+
+    expect(page).toHaveProperty('url_alias.url_path', '/page/some-about-to-be-deleted-new-page')
+    expect(page).toHaveProperty('url_alias.generated', true);
+
+    await strapi.entityService.delete("api::test.test", page.id);
+
+    const alias = await strapi.entityService.findOne("plugin::webtools.url-alias", page.url_alias.id);
+
+    expect(alias).toBe(null);
+  });
+});
---
+++ playground/src/api/private-category/routes/private-category.ts
@@ -0,0 +1,7 @@
+/**
+ * private-category router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::private-category.private-category');
