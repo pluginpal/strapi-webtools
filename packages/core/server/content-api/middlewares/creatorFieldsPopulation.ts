49fe9bc05fc2799735e92a76dd8639982aa04680
Boaz Poolman <boaz_skater@hotmail.com>
2024-03-28 08:38:39 +0100

    chore: remove the creator fields middleware

--- packages/core/server/content-api/middlewares/creatorFieldsPopulation.ts
+++
@@ -1,9 +0,0 @@
-import { Context, Next } from 'koa';
-
-export default async (ctx: Context, next: Next) => {
-  if (!ctx.query.populate) {
-    ctx.query.populate = ['createdBy', 'updatedBy'];
-  }
-
-  await next();
-};
