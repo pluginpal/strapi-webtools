84b951c08b055eada26166fa240715ee25d7061a
Boaz Poolman <boaz_skater@hotmail.com>
2024-05-17 10:05:12 +0200

    fix: cypress test

--- packages/core/admin/index.cy.tsx
+++ packages/core/admin/index.cy.tsx
@@ -4,6 +4,6 @@
 describe('Webtools Core', () => {
   it('Load the homepage and check if somethings there', () => {
     cy.visit('/');
-    cy.contains('The server is running successfully');
+    cy.contains("Let's get started!");
   });
 });
