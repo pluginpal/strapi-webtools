a106a41d9c828d030a1ee689af425f8e04e99799
Boaz Poolman <boaz_skater@hotmail.com>
2024-05-17 10:08:29 +0200

    style: fix eslint issue

--- packages/core/admin/index.cy.tsx
+++ packages/core/admin/index.cy.tsx
@@ -4,6 +4,7 @@
 describe('Webtools Core', () => {
   it('Load the homepage and check if somethings there', () => {
     cy.visit('/');
+    // eslint-disable-next-line quotes
     cy.contains("Let's get started!");
   });
 });
