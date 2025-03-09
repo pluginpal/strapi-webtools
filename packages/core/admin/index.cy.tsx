61ec68bd5cf657b5fc33471cc658ca2cfcd6af35
Boaz Poolman <boaz_skater@hotmail.com>
2025-03-09 22:06:49 +0000

    fix: e2e tests

--- packages/core/admin/index.cy.tsx
+++ packages/core/admin/index.cy.tsx
@@ -5,6 +5,6 @@ describe('Webtools Core', () => {
   it('Load the homepage and check if somethings there', () => {
     cy.visit('/');
     // eslint-disable-next-line quotes
-    cy.contains("Let's get started!");
+    cy.contains("Welcome to Strapi");
   });
 });
