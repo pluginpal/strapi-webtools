1892da5a602c038c93a009be5cdef89004874c57
Jorrit Schippers <jorrit@ncode.nl>
2025-07-07 09:20:26 +0200

    chore: simplify isContentTypeEnabled

--- packages/addons/sitemap/server/utils/enabledContentTypes.ts
+++ packages/addons/sitemap/server/utils/enabledContentTypes.ts
@@ -1,20 +1,8 @@
 import get from 'lodash/get';
-import { UID, Schema } from '@strapi/strapi';
+import { Schema } from '@strapi/strapi';
 
 
-export const isContentTypeEnabled = (ct: UID.ContentType) => {
-  let contentType: Schema.ContentType;
-
-  if (typeof ct === 'string') {
-    contentType = strapi.contentTypes[ct];
-  } else {
-    contentType = ct;
-  }
-
+export const isContentTypeEnabled = (contentType: Schema.ContentType) => {
   const { pluginOptions } = contentType;
-  const enabled = get(pluginOptions, ['webtools', 'enabled'], false) as boolean;
-
-  if (!enabled) return false;
-
-  return true;
+  return get(pluginOptions, ['webtools', 'enabled'], false) as boolean;
 };
