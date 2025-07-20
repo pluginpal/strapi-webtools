1b8e355fe1b72a9e9633d4eecd725e765992881a
Jorrit Schippers <jorrit@ncode.nl>
2025-07-20 16:13:09 +0200

    chore: simplify isContentTypeEnabled in core

--- packages/core/server/util/enabledContentTypes.ts
+++ packages/core/server/util/enabledContentTypes.ts
@@ -1,18 +1,8 @@
 import get from 'lodash/get';
 import { Schema } from '@strapi/strapi';
 
-import { pluginId } from './pluginId';
 
-export const isContentTypeEnabled = (ct: Schema.ContentType) => {
-  let contentType: Schema.ContentType;
-
-  if (typeof ct === 'string') {
-    contentType = strapi.contentTypes[ct];
-  } else {
-    contentType = ct;
-  }
-
+export const isContentTypeEnabled = (contentType: Schema.ContentType) => {
   const { pluginOptions } = contentType;
-
-  return get(pluginOptions, [pluginId, 'enabled'], false) as boolean;
+  return get(pluginOptions, ['webtools', 'enabled'], false) as boolean;
 };
